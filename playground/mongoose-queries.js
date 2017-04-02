25e63583aee16c607a78aae216eb38957cc8fd83
Jon Linton <jon@studio-424.com>
2017-04-01 23:35:34 -0500

    GET /todos/:id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,45 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+ var id = '58df2b658e7eccdcbf50bdad';
+
+ if(!ObjectID.isValid(id)){
+ 	console.log('ID is not valid');  
+ }
+
+ // if(!ObjectID.isValid(id)){
+ // 	console.log('ID not valid');  
+ // }
+
+ // Todo.find({
+ // 	_id: id
+ // }).then((todos) => {
+ // 	console.log('todos: ', todos);
+ // });
+
+ // Todo.findOne({
+ // 	_id: id
+ // }).then((todo) => {
+ // 	console.log('todo: ', todo);
+ // }); 
+
+// Todo.findById(id).then((todo) => {
+// 	if(!todo){
+// 		return console.log('Id not found');
+// 	}
+// 	console.log('Todo: ', todo);
+// }).catch((e) => {
+// 	console.log(e);
+// });
+
+User.findById(id).then((user) => {
+	if(!user){
+		console.log('User not found');
+	}
+	console.log('User: ', user)
+}).catch((e) => {
+	console.log(e);
+});
