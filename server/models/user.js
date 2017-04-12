dd1e3de03368f1f7243d29724c4ea839b185653a
Jon Linton <jon@studio-424.com>
2017-04-12 08:49:04 -0500

    add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,18 @@ UserSchema.methods.generateAuthToken = function () {
 	});
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens: {
+				token: token
+			}
+		}
+	});
+};
+
 UserSchema.statics.findByToken = function(token) {
 	var User = this;
 	var decoded;
@@ -89,7 +101,7 @@ UserSchema.statics.findByCredentials = function(email, password) {
 					//return user.generateAuthToken();
 					resolve(user);
 				} else {
-					reject(err);
+					reject();
 				}
 			});
 
