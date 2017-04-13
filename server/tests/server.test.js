c1d35e89fa2377f22c1ef8a6aa71b91ba3d58c02
Jon Linton <jon@studio-424.com>
2017-04-13 06:03:42 -0500

    add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -301,3 +301,34 @@ describe('POST /users/login', () => {
 		});
 	});
 });
+
+describe('DELETE /users/me/token', () => {
+	it('should remove auth token on logout', (done) => {
+		var _id = users[0]._id;
+		var token = users[0].tokens[0].token;
+
+		request(app)
+		.delete('/users/me/token')
+		.set('x-auth', token)
+		.expect(200)
+		.end((err, res) => {
+			if(err){
+				return done(err);
+			}
+			User.findById(_id).then((user) => {
+				expect(user.tokens.length).toBe(0);
+				done();
+			}).catch((e) => {
+				done(e);
+			});
+		});
+
+
+
+	});
+});
+
+
+
+
+
